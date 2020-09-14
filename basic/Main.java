a39104063dabdaef1504f3532da55ad04c3dcd93
Matthew <Mattpet26@gmail.com>
2020-09-14 16:03:43 -0700

    finished timer per sec

--- basic/Main.java
+++ basic/Main.java
@@ -1,5 +1,10 @@
+import java.time.LocalDateTime;  
+import java.time.format.DateTimeFormatter;  
+
 public class Main {
   public static void main(String[] args){
+    flipNHeads(3);
+    clock();
     int dogCount = 1;
     System.out.println("I own " + dogCount + " " + pluralize("dog", dogCount) + ".");
 
@@ -14,5 +19,34 @@ public class Main {
       return word + "s";
     }
     return word;
+  }
+  public static void flipNHeads(int n){
+    int heads = 0;
+    int count = 0;
+    while(heads < n){
+      double flip = Math.random();
+      if(flip >= 0.5) {
+        System.out.println("Heads");
+        heads++;
+      } else {
+        System.out.println("Tails");
+        heads = 0;
+      }
+      count ++;
+      }
+    System.out.println("It took " + count + " flips to achieve three heads in a row");
+  }
+  public static void clock(){
+    String test ="";
+    while(true){
+      LocalDateTime now = LocalDateTime.now();
+      DateTimeFormatter format = DateTimeFormatter.ofPattern("dd-MM-YYY HH:mm:ss");
+      String formatDateTime = now.format(format);
+
+      if(!test.equals(formatDateTime)){
+        System.out.println(formatDateTime);
+      }
+      test = formatDateTime;
+    }
   }
 }
